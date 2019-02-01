1fef8f8160d2f2d9943580debf24ba27fe8bde6a
Anya Snow <emailasnow@gmail.com>
2019-01-31 21:24:22 -0800

    files

---
+++ index.js
@@ -0,0 +1,16 @@
+//function catCarousel(){
+$('.thumbnails').on("click", "img", function(event){
+
+console.log(this.src);
+// 	let clickedCatUrl = $(this).attr('src');
+// 	let clickedCatAlt = $(this).attr('alt');
+
+//   $('.hero').attr('src', clickedCatUrl);
+//   $('.hero').attr('alt', clickedCatAlt);
+
+// console.log(clickedCatUrl)
+});
+//};
+
+
+//$(catCarousel);
---
+++ script.js
@@ -0,0 +1,16 @@
+function catCarousel(){
+$('.thumbnail').click(function(event){
+
+console.log(this.src);
+// 	let clickedCatUrl = $(this).attr('src');
+// 	let clickedCatAlt = $(this).attr('alt');
+
+//   $('.hero').attr('src', clickedCatUrl);
+//   $('.hero').attr('alt', clickedCatAlt);
+
+// console.log(clickedCatUrl)
+});
+};
+
+
+$(catCarousel);
