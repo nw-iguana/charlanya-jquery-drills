c841d55d35fdd79379549730bdfd8a3b4989e7e5
Charlotte Shih <shih.charlotte@gmail.com>
2019-01-31 23:05:17 -0800

    fizzbuzz drill

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-$('.thumbnails').on("click", "img", function(event){
+$('.thumbnail img').on('click', function(event){
     $('.hero').html(`<img src="${this.src}" alt="${this.alt}">`);
 });
 
 
-$('.thumbnails').keypress(function(event){
-	let keycode = (event.keyCode ? event.keyCode : event.which);
-    if(keycode == '13'){
-        $('.hero').html(`<img src="${this.src}" alt="${this.alt}">`);	
-    }
-});
+
+// function handleThumbnailClicks() {
+//     $('.thumbnail').on('click', function(event) {
+//       const heroImageSrc = $(this).find('img').attr('src');
+//       const heroImageAlt = $(this).find('img').attr('alt');
+  
+//       $('.hero img').attr('src', heroImageSrc).attr('alt', heroImageAlt);
+//     });
+//   }
+  
+// $(handleThumbnailClicks);
+
+
+// $('.thumbnail').keypress(function(event){
+// 	let keycode = (event.keyCode ? event.keyCode : event.which);
+//     if(keycode == '13'){
+//         $('.hero').html(`<img src="${this.src}" alt="${this.alt}">`);	
+//     }
+// });
