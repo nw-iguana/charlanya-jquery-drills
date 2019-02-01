7d0f2d0c6bb2f2d9959bb781e56e1862444be9c7
Charlotte Shih <shih.charlotte@gmail.com>
2019-01-31 21:42:37 -0800

    cat carousel

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-//function catCarousel(){
 $('.thumbnails').on("click", "img", function(event){
-
-console.log(this.src);
-// 	let clickedCatUrl = $(this).attr('src');
-// 	let clickedCatAlt = $(this).attr('alt');
-
-//   $('.hero').attr('src', clickedCatUrl);
-//   $('.hero').attr('alt', clickedCatAlt);
-
-// console.log(clickedCatUrl)
+    $('.hero').html(`<img src="${this.src}" alt="${this.alt}">`);
 });
-//};
 
 
-//$(catCarousel);
+$('.thumbnails').keypress(function(event){
+	let keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13'){
+        $('.hero').html(`<img src="${this.src}" alt="${this.alt}">`);	
+    }
+});
